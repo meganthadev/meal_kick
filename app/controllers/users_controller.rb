bab74ea6f125a022e946798d39d38203f170e244
meganthadev <meganthadev@gmail.com>
2020-07-12 16:47:43 -0700

    Add show action to users contr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     end 
 
     def show 
+        redirect_if_not_logged_in
         @user = User.find_by(params[:id])
         redirect_to '/' if !@user
     end 
