a4e991503624dd14628b844ce08bb9b670741b0d
meganthadev <meganthadev@gmail.com>
2020-07-12 14:48:15 -0700

    Add create action/sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,24 @@
 class SessionsController < ApplicationController 
 
+    def home
+    end 
+
+    def create 
+       user = User.find_by(email: params[:user][:email])
+       if user && user.authenticate(params[:user][:password])
+        session[:user_id] = user.id 
+        redirect_to user_path(user)
+       else 
+        flash[:message] = "Incorrect login credentials. Please try again."
+        redirect_to "/login"
+       end 
+    end 
+
+    def destroy 
+      session.clear
+      redirect_to root_path
+    end 
+
 
 
 end  
