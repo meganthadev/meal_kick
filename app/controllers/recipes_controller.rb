fca469382bd086cccd87125c0b6b0d3ad88aad42
meganthadev <meganthadev@gmail.com>
2020-07-12 16:46:51 -0700

    Add before_action to recipes contr

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,14 @@
 class RecipesController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def new 
+    end 
+
+    def create 
+    end 
+    
+    def index 
+    end   
+
+
 end
