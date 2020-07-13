ad2c389dbbd8958e196d1a331e41e61af28f81bd
meganthadev <meganthadev@gmail.com>
2020-07-12 22:49:26 -0700

    Update new recipe redirect rte

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,13 +2,32 @@ class RecipesController < ApplicationController
     before_action :redirect_if_not_logged_in
 
     def new 
+        @recipe = Recipe.new
     end 
 
     def create 
-    end 
+        @recipe = current_user.recipes.build(recipe_params)
+         #if @recipe.save 
+            render :'recipes/show'
+        #else 
+            #render :new
+        #end 
+     end 
+
+     def show 
+        @recipe = Recipe.find_by_id(params[:id])
+        redirect_to recipes_path if !@recipe
+     end  
     
     def index 
-    end   
+        @recipes = Recipe.all
+      end   
+
+    private 
+
+    def recipe_params
+        params.require(:recipe).permit(:title, :description, :instructions)
+    end 
 
 
 end
