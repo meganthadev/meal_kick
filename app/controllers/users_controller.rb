673c2620ce99c692562f336d0b85db7ddf1f94a4
meganthadev <meganthadev@gmail.com>
2020-07-13 00:44:09 -0700

    Update create action user contr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
     def show 
         redirect_if_not_logged_in
-        @user = User.find_by(params[:id])
+        @user = User.find_by_id(params[:id])
         redirect_to '/' if !@user
     end 
 
