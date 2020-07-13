da3680dadd9613f27e44c4080df11772be1da447
meganthadev <meganthadev@gmail.com>
2020-07-12 22:53:21 -0700

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
     has_many :recipes 
     has_many :comments 
-    has_many  :commented_recipes, through: :comments, source: :recipe 
+    has_many  :recipe_comments, through: :comments, source: :recipe 
     has_secure_password
     
+    
+
 
 end
