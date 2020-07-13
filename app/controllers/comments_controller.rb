b5791136cbeb2cd7bf52261549eb12a42bf6e353
meganthadev <meganthadev@gmail.com>
2020-07-13 00:46:52 -0700

    Add CRUD routes to cmmnt contr

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,23 @@ class CommentsController < ApplicationController
         @comment = Comment.new
     end
 
+    def index 
+        @comments = Comment.all 
+    end     
+
+    def show 
+    end 
+
+    def delete
+        @comment.destroy
+        redirect_to root_path
+    end  
+    
+    
+    private 
+
+    def comment_params
+        params.require(:comment).permit(:content, :user_id, :recipe_id)
+    end 
+
 end 
