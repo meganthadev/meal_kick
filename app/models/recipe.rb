fb52513ee1a653751fa8100c016f6dcdb2c88fe8
meganthadev <meganthadev@gmail.com>
2020-07-13 16:57:31 -0700

    Add validations to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  belongs_to :category
+  has_many :comments, dependent: :destroy 
   has_many :users, through: :comments
+  validates :title, presence: true, length: { in: 3..30 }
+  validates :description, presence: true, length: { in: 15..100 }
+  validates :instructions, presence: true, length: { maximum: 750 }
+  default_scope { order(created_at: :desc)}
 end
