a24814aa9dfd9d06edab04bc3feac40139f05b19
meganthadev <meganthadev@gmail.com>
2020-07-13 17:01:26 -0700

    Update helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-    helper_method :current_user, :logged_in?
+    helper_method :current_user, :logged_in?, :redirect_if_not_logged_in
 
   private
 
@@ -15,6 +15,7 @@ class ApplicationController < ActionController::Base
     def redirect_if_not_logged_in
         redirect_to '/' if !logged_in?
     end  
+
 
     
 
