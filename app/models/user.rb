92f30e9a0cd2fe06724869a40310bef537228be2
meganthadev <meganthadev@gmail.com>
2020-07-13 21:49:29 -0700

    Update user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
     has_many :comments 
     has_many  :recipe_comments, through: :comments, source: :recipe 
     has_secure_password
+    validates :username, presence: true, length: { in: 3..20 }
+    validates :email, uniqueness: true
+    validates :email, presence: true 
+    
     
     
 
