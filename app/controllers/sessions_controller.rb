001811c0b632225f0d95fe0577f13c01fd658fea
meganthadev <meganthadev@gmail.com>
2020-07-13 21:51:22 -0700

    Edit sessions actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
     end 
 
     def create 
-       user = User.find_by(username: params[:user][:username])
-       if user && user.authenticate(params[:user][:password])
-        session[:user_id] = user.id 
-        redirect_to user_path(user)
+       @user = User.find_by(email: params[:email])
+       if @user
+        session[:user_id] = @user.id 
+        redirect_to user_path(@user)
        else 
         flash[:message] = "Incorrect login credentials. Please try again."
         redirect_to "/login"
