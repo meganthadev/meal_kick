0c7fe8fd5240196e1cebbee67c190c166d222818
meganthadev <meganthadev@gmail.com>
2020-07-14 23:32:56 -0700

    Refactor helper methods/migrate to helper folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,13 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    helper_method :current_user, :logged_in?, :redirect_if_not_logged_in
+    include ApplicationHelper
 
-    def logged_in?
-        !!current_user
-    end
 
-    def show 
-        @user = User.find_by(id: params[:id])
-    end 
+  
     
-  private
 
-    def current_user
-        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-    end 
 
-    
-    def redirect_if_not_logged_in
-        redirect_to '/' if !logged_in?
-    end  
 
-    def authorized_to_edit_recipe?(recipe)
-        recipe.user == current_user
-    end
-
-    def authorized_to_edit_cmnt?(comment)
-        comment.user == current_user
-    end
-    
+   
 
 end
