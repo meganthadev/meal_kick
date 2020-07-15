f10eaa949163171e566c00995aa8b3701062e69a
meganthadev <meganthadev@gmail.com>
2020-07-14 21:58:48 -0700

    Add actions to cat contr

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,9 +7,11 @@ class CategoriesController < ApplicationController
     end 
     
     def index 
+        @categories = Category.all 
     end 
 
     def show 
+        @category = Category.find_by(id: params[:id])
     end   
 
 
