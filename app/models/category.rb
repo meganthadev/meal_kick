abeb0bb4a25dcbe971627dad2f4f3b05d3df30bb
meganthadev <meganthadev@gmail.com>
2020-07-14 21:57:32 -0700

    Updaet category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
     has_many :recipes
+    has_many :users, through: :recipes
     validates :title, presence: true, length: { maximum: 20 } 
     default_scope { order(created_at: :desc)}
 end
