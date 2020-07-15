165a43249b63dd23a36bde39273b43e2cd935395
meganthadev <meganthadev@gmail.com>
2020-07-14 23:27:16 -0700

    Updated recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,9 +3,17 @@ class Recipe < ApplicationRecord
   belongs_to :category
   has_many :comments, dependent: :destroy 
   has_many :users, through: :comments
+
   validates :title, presence: true, length: { in: 3..30 }
   validates :description, presence: true, length: { in: 15..100 }
   validates :instructions, presence: true, length: { maximum: 750 }
   default_scope { order(created_at: :desc)}
+  
   accepts_nested_attributes_for :comments
+
+  def category_attributes(attr)
+    self.category = Category.find_or_create_by(attr) if !attr[:name].blank? 
+  end 
+
+
 end
