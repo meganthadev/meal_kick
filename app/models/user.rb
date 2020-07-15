980825e2a5cb136f5ad35ec7f45a7e5de471aa88
meganthadev <meganthadev@gmail.com>
2020-07-14 23:26:48 -0700

    Add attr method to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,13 @@ class User < ApplicationRecord
     has_many :comments 
     has_many :recipe_comments, through: :comments, source: :recipe 
     has_many :categories, through: :recipes
+    
     has_secure_password
+    
     validates :username, presence: true, length: { in: 3..18 }
     validates :email, presence: true 
     validates :email, uniqueness: true
    
-    
-    
-    def authored_this(com)
-        if com.class == Recipe && com.user_id == self.id 
-          true
-        elsif com.class == Comment && com.user_id == self.id 
-          true
-        else
-          false
-        end
-      end
 
 
 end
