b1e77a4e6e8eca4247c269449af18c8d49c8d751
meganthadev <meganthadev@gmail.com>
2020-07-14 21:57:07 -0700

    Update user model with auth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,24 @@
 class User < ApplicationRecord
     has_many :recipes 
     has_many :comments 
-    has_many  :recipe_comments, through: :comments, source: :recipe 
+    has_many :recipe_comments, through: :comments, source: :recipe 
+    has_many :categories, through: :recipes
     has_secure_password
-    validates :username, presence: true, length: { in: 3..20 }
-    validates :email, uniqueness: true
+    validates :username, presence: true, length: { in: 3..18 }
     validates :email, presence: true 
+    validates :email, uniqueness: true
+   
     
     
-    
+    def authored_this(com)
+        if com.class == Recipe && com.user_id == self.id 
+          true
+        elsif com.class == Comment && com.user_id == self.id 
+          true
+        else
+          false
+        end
+      end
 
 
 end
