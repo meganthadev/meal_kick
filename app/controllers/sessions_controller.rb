506d838ca4b553c7dffa3bae0f09252680f95c43
meganthadev <meganthadev@gmail.com>
2020-07-14 23:30:22 -0700

    Update new action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,28 @@
 class SessionsController < ApplicationController 
 
+    def new 
+      @user = User.new
+      render :login
+    end 
+
     def home
     end 
 
     def create 
-       @user = User.find_by(email: params[:email])
-       if @user
+       @user = User.find_by(email: params[:user][:email])
+       if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id 
         redirect_to user_path(@user)
        else 
-        flash[:message] = "Incorrect login credentials. Please try again."
-        redirect_to "/login"
+        flash[:error] = "Incorrect login credentials. Please try again."
+        redirect_to '/login'
        end 
     end 
 
     def destroy 
       session.clear
-      redirect_to root_path
+      redirect_to '/'
     end 
-
 
 
 end  
