42882a5fb900d0e88ecee3fbaa89f2b25c593999
meganthadev <meganthadev@gmail.com>
2020-07-16 16:59:10 -0700

    update app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,38 +1,5 @@
 module ApplicationHelper
-
-    def current_user
-        @current_user ||= User.find_by_id(session[:user_id])
-    end
-    
-    def authored_this(com)
-        if com.class == Recipe && com.user_id == self.id 
-          true
-        elsif com.class == Comment && com.user_id == self.id 
-          true
-        else
-          false
-        end
-    end
-
-    def logged_in?
-        !!session[:user_id]
-    end
-
-    def current_user
-        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-    end 
-
-    def redirect_if_not_logged_in
-        redirect_to '/' if !logged_in?
-    end  
-
-    def authorized_to_edit_recipe?(recipe)
-        recipe.user == current_user
-    end
-
-    def authorized_to_edit_cmnt?(comment)
-        comment.user == current_user
-    end
-    
-
 end
+   
+    
+    
