a925523bb28f023927f5a87bdcbc0a0969e6ec15
meganthadev <meganthadev@gmail.com>
2020-07-16 01:17:31 -0700

    Update reipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,16 +4,24 @@ class Recipe < ApplicationRecord
   has_many :comments, dependent: :destroy 
   has_many :users, through: :comments
 
+  delegate :name, to: :category
+  
   validates :title, presence: true, length: { in: 3..30 }
   validates :description, presence: true, length: { in: 15..100 }
   validates :instructions, presence: true, length: { maximum: 750 }
-  default_scope { order(created_at: :desc)}
+  scope :alpha, -> { order(:title) }
   
-  accepts_nested_attributes_for :comments
+  accepts_nested_attributes_for :users
 
   def category_attributes(attr)
-    self.category = Category.find_or_create_by(attr) if !attr[:name].blank? 
+    category = Category.find_or_create_by(attr)
+    self.category = category if category.valid? || !self.category 
   end 
+
+  def user_attributes=(attr)
+    user = User.find_or_create_by(attr)
+    self.user = user if user.valid? || !self.user
+  end
 
 
 end
