b82c7aaa8b6b7eba6b2cac1e3918772309c689ce
meganthadev <meganthadev@gmail.com>
2020-07-16 01:17:10 -0700

    Add scope methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,19 @@ class User < ApplicationRecord
     has_many :recipe_comments, through: :comments, source: :recipe 
     has_many :categories, through: :recipes
     
-    has_secure_password
+    has_secure_password #(authenticate, validate password, password confirmation)
     
     validates :username, presence: true, length: { in: 3..18 }
     validates :email, presence: true 
     validates :email, uniqueness: true
    
+    def self.most_recipes
+        joins(:recipes).group(:user_id).order("count(user_id) DESC").limit(3)
+        # combine with recipes  - JOIN
+        # count no. of posts
+        # sort desc  - ORDER
+    end
 
+    
 
 end
