17b53d0beb7ca9de67b82604207fa0d045a42f73
meganthadev <meganthadev@gmail.com>
2020-07-16 01:20:09 -0700

    Add params to cat contr

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,18 +1,32 @@
 class CategoriesController < ApplicationController
+    before_action :redirect_if_not_logged_in
 
     def new 
+     @category = Category.new
     end 
-    
+
     def create 
-    end 
+        @category = current_user.recipes.build(category_params)
+        if @category.save
+          redirect_to category_path(@category)
+        else
+          render :new
+        end
+    end  
+    
     
     def index 
-        @categories = Category.all 
+        @categories = Category.all.includes(:recipes)
     end 
 
     def show 
-        @category = Category.find_by(id: params[:id])
+        @category = Category.find_by(params[:id])
     end   
+    
+    private 
 
+    def category_params
+        params.require(:category).permit(:name, :category_id)
+    end 
 
 end
