00326e836c2f79159ac674c5cd83532067e07d59
meganthadev <meganthadev@gmail.com>
2020-07-16 01:17:53 -0700

    Update comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,11 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :recipe
-    validates :content, presence: true, length: { maximum: 250 } 
+    
+    validates :content, presence: true, length: { maximum: 200 } 
     default_scope { order(created_at: :desc)}
+
+    validates :recipe, uniqueness: {scope: :user_id, message: "may only have one comment per user"}
     
     
 end
