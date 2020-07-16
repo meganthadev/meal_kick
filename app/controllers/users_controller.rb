bc4fbfcd903b17c95c3aa6f2089545294b2fca2d
meganthadev <meganthadev@gmail.com>
2020-07-16 01:18:51 -0700

    Add profile method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save 
            session[:user_id] = @user.id
-           redirect_to @user 
+           redirect_to '/profile' 
         else  
             render :new 
         end 
@@ -19,19 +19,31 @@ class UsersController < ApplicationController
     end 
 
     def show 
+        redirect_if_not_logged_in
+        @user = User.includes(recipes: :category).find_by_id(params[:id])
         redirect_to '/' if !@user
-        @user = User.find_by(id: params[:id]) 
     end 
 
-    def index 
-        @user = User.all
-    end  
+    def profile
+        if logged_in?
+        @recipes = Recipe.where(user_id: current_user.id).order(title: :desc)
+        @categories = Category.where(user_id: current_user.id).order(name: :desc)
+        @user = User.find_by(id: current_user.id)
+        @comments = @user.recipe_comments.order(created_at: :desc)
+        render :show
+        else
+          redirect_to '/'
+        end
+      end
 
+      def most_recipes
+        @users = User.most_recipes
+      end
     
     private 
 
     def user_params
-        params.require(:user).permit(:username, :email, :password)
+        params.require(:user).permit(:username, :email, :password, :user_id)
     end  
     
 end
