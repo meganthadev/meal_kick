63e19c67410f954838281610e9f8a71315519ee0
meganthadev <meganthadev@gmail.com>
2020-07-16 17:01:03 -0700

    Update user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     end 
 
     def create 
-        @user = User.new(user_params)
+      @user = User.new(user_params)
         if @user.save 
            session[:user_id] = @user.id
            redirect_to '/profile' 
@@ -20,21 +20,23 @@ class UsersController < ApplicationController
 
     def show 
         redirect_if_not_logged_in
-        @user = User.includes(recipes: :category).find_by_id(params[:id])
+        @user = User.find_by(id: params[:id])
+        #@user = User.includes(recipes: :category).find_by_id(params[:id])
         redirect_to '/' if !@user
     end 
 
     def profile
         if logged_in?
-        @recipes = Recipe.where(user_id: current_user.id).order(title: :desc)
-        @categories = Category.where(user_id: current_user.id).order(name: :desc)
-        @user = User.find_by(id: current_user.id)
-        @comments = @user.recipe_comments.order(created_at: :desc)
-        render :show
+         @recipes = Recipe.where(user_id: current_user.id).order(title: :desc)
+         @categories = Category.where(user_id: current_user.id).order(name: :desc)
+         @user = User.find_by(id: current_user.id)
+         @comments = @user.recipe_comments.order(created_at: :desc)
+         render :show
         else
           redirect_to '/'
         end
-      end
+    end 
+      
 
       def most_recipes
         @users = User.most_recipes
@@ -43,7 +45,7 @@ class UsersController < ApplicationController
     private 
 
     def user_params
-        params.require(:user).permit(:username, :email, :password, :user_id)
+        params.require(:user).permit(:username, :email, :password)
     end  
     
 end
