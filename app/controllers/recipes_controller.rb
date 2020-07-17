3b6934cfea1749a544c5d69661134a4416f92895
meganthadev <meganthadev@gmail.com>
2020-07-16 22:50:52 -0700

    Update recipe_params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ class RecipesController < ApplicationController
     before_action :set_recipe, only: [:show, :edit, :update, :destroy]
     before_action :redirect_if_not_logged_in
 
+
     def new 
       if params[:user_id] && @user = User.find_by_id(params[:user_id])
         @recipe = @user.recipes.build
@@ -9,38 +10,21 @@ class RecipesController < ApplicationController
         @recipe = Recipe.new
       end
       @recipe.build_category
-    end
-      #  @recipe = Recipe.new
-     #   @comment = Comment.new
-      #  @category = Category.new
-      #  @comment.recipe_id = @recipe.id 
-      # end 
-   
+    end   
 
     def create 
-     # @user = User.find_by(params[:user_id])
-     @recipe = current_user.recipes.build(recipe_params)
+    @recipe = current_user.recipes.build(recipe_params)
     if @recipe.save
-      flash[:message] = "Recipe saved!"
+      flash[:notice] = "Recipe saved!"
       redirect_to recipes_path
     else
       render :new
     end
   end
 
-  #    @user.recipes.build(params[:user][:recipe])
-   #   @recipe = current_user.recipes.build(recipe_params)
-    #    if @recipe.save
-     #     flash[:notice] = "Recipe saved!"
-      #    redirect_to recipe_path(@recipe)
-       #   binding.pry
-        ##  render :new
-        #end 
-     #end 
-
-     def show 
-        @comment = Comment.new 
-     end  
+    def show 
+     @comment = Comment.new 
+    end  
 
      def update
         if @recipe.update(recipe_params)
@@ -62,7 +46,7 @@ class RecipesController < ApplicationController
     private 
 
     def recipe_params
-        params.require(:recipe).permit(:title, :description, :instructions, :user_id, :category_id, category_attributes: [:name])
+        params.require(:recipe).permit(:title, :description, :instructions, :category_id, category_attributes: [:name])
     end 
 
     def set_recipe
