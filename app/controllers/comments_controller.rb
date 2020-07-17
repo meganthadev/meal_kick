fc438797fb0ea94230b8ed1c6ff3c0e2e9c3d2f7
meganthadev <meganthadev@gmail.com>
2020-07-16 22:52:11 -0700

    Update comment contr

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,39 @@
 class CommentsController < ApplicationController
-    before_action :get_comment, only: [:edit, :update, :destroy]
-    before_action :set_comments, only: [:edit, :update, :destroy]
+    before_action :set_comment, only: [:show, :edit, :update, :destroy]
     before_action :redirect_if_not_logged_in
+    before_action :redirect_if_not_comment_author, only: [:edit, :update, :destroy]
 
 
     def new
-        @comment = Comment.new 
+        if params[:recipe_id] && @recipe = Recipe.find_by_id(params[:recipe_id])
+            @comment = @recipe.comments.build
+          else
+            @error = "That recipe doesn't exist" if params[:recipe_id]
+            @comment = Comment.new
+          end
     end 
 
     def create
         @comment = current_user.comments.build(comment_params)
         if @comment.save
-            redirect_to comments_path
+            redirect_to comment_path(@comment)
         else
         render :new
         end
     end 
 
+    def show 
+    end     
+
     def edit
     end 
 
     def update
-        @comment.update(comment_params)
-        redirect_to recipe_path(@recipe.id)
+        if @comment.update(comment_params)
+            redirect_to comment_path(@comment)
+        else
+            render :edit
+         end
     end 
 
     def index 
@@ -41,21 +52,20 @@ class CommentsController < ApplicationController
 
     private 
 
-    def get_comment
-        @comment = Comment.find_by(id: params[:id])
-    end 
-
-    def set_comments
-        @recipe = @comment.recipe 
-        @comments = @recipe.comments.find_by(id: params[:id])
-    end 
-        
     def comment_params
-        params.require(:comment).permit(:content, :recipe_id)
-    end 
+        params.require(:comment).permit(:content,:recipe_id)
+      end
 
-    def authorized_on_recipe?(recipe)
-        recipe.user == current_user
-    end 
+    def set_comment
+        @comment = Comment.find_by(id: params[:id])
+        if !@comment
+          flash[:message] = "Comment was not found"
+          redirect_to comments_path
+        end
+      end
+    
+      def redirect_if_not_comment_author
+         redirect_to comments_path if @comment.user != current_user
+      end
 
 end
