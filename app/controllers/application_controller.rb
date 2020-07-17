9b00c27392599b152ef763eebf1b696b78ea60e8
meganthadev <meganthadev@gmail.com>
2020-07-16 22:52:55 -0700

    Update app contr, delete arbitrary code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,23 +17,8 @@ class ApplicationController < ActionController::Base
       redirect_to '/' if !logged_in?
    end
 
-   def authored_this(com)
-        if com.class == Recipe && com.user_id == self.id 
-            true
-        elsif com.class == Comment && com.user_id == self.id 
-            true
-        else
-            false
-        end
-    end
 
-    def authorized_to_edit_recipe?(recipe)
-        recipe.user == current_user
-    end
+    
 
-    def authorized_to_edit_cmnt?(comment)
-        comment.user == current_user
-    end
-   
 
 end
