ff3615fb865972124040710bbcd86ab329d42436
meganthadev <meganthadev@gmail.com>
2020-07-16 17:01:59 -0700

    Update session create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,8 @@ class SessionsController < ApplicationController
     end 
 
     def create 
-       @user = User.find_by(email: params[:user][:email])
-       if @user && @user.authenticate(params[:user][:password])
+       user = User.find_by(email: params[:user][:email])
+       if user && user.authenticate(params[:user][:password])
         session[:user_id] = @user.id 
         redirect_to user_path(@user)
        else 
@@ -23,5 +23,25 @@ class SessionsController < ApplicationController
       redirect_to '/'
     end 
 
+    def google
+      #find_or_create a user using the attributes auth
+      @user = User.find_or_create_by(email: auth["info"]["email"]) do |user|
+        user.username = auth["info"]["first_name"]
+        user.password = SecureRandom.hex(10)
+      end
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        redirect_to '/'
+      end
+    end
+
+    private
+
+    def auth
+      request.env['omniauth.auth']
+    end
+  
 
 end  
