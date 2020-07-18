d1133ccab74dcec5228df9b968d01790314fc224
meganthadev <meganthadev@gmail.com>
2020-07-17 18:09:32 -0700

    Update valid on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     has_many :recipes 
     has_many :comments 
-    has_many :recipe_comments, through: :comments, source: :recipe 
+    has_many :commented_recipes, through: :comments, source: :recipe 
     has_many :categories, through: :recipes
-    
-    has_secure_password #(authenticate, validate password, password confirmation)
+
+    has_secure_password 
     
     validates :username, presence: true, length: { in: 3..18 }
     validates :email, presence: true 
@@ -12,9 +12,6 @@ class User < ApplicationRecord
    
     def self.most_recipes
         joins(:recipes).group(:user_id).order("count(user_id) DESC").limit(3)
-        # combine with recipes  - JOIN
-        # count no. of posts
-        # sort desc  - ORDER
     end
 
     
